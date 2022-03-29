5dbb7d820d924470bed826b3e1cc3456dfd4733f
Aron Soppe <a.soppe@emit-it.com>
2022-03-29 15:19:28 +0200

    feat: adds early return statements when captcha verification is not valid

--- server/controllers/submit-controller.js
+++ server/controllers/submit-controller.js
@@ -6,15 +6,16 @@ module.exports = {
     // Checks if there is a captcha provider
     if (!(strapi.config.get('plugin.ezforms.captchaProvider.name') === 'none') && (strapi.config.get('plugin.ezforms.captchaProvider.name'))) {
       verification = await strapi.plugin('ezforms').service(strapi.config.get('plugin.ezforms.captchaProvider.name')).validate(ctx.request.body.token)
+
       //throws error if invalid
       if (!verification.valid) {
         strapi.log.error(verification.error)
-        if (verification.code === 500) {
-          ctx.internalServerError("There was an error, check Strapi logs for more details. " + verification.message)
+        if (verification.error.code === 500) {
+          return ctx.badRequest("ReCAPTCHA verification has failed: " + verification.error.message)
         } else if (verification.code === 400) {
-          ctx.badRequest(verification.message)
+          return ctx.badRequest(verification.error.message)
         } else {
-          ctx.internalServerError("There was an error")
+          return ctx.internalServerError("Internal server error")
         }
       }
     }
@@ -26,7 +27,7 @@ module.exports = {
           await strapi.plugin('ezforms').service(provider.name).send(provider.config, ctx.request.body.formData)
         } catch (e) {
           strapi.log.error(e)
-          ctx.internalServerError('A Whoopsie Happened');
+          return ctx.internalServerError('An unexpected error has occurred');
         }
       }
     }
