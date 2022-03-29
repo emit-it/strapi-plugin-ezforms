43c388cb1db997e47cc68f3c007b2a3fd09ff4e6
Aron Soppe <a.soppe@emit-it.com>
2022-03-29 15:20:17 +0200

    feat: adds better readability for HTML emails sent by ezforms per form property

--- server/services/notification-providers/email.js
+++ server/services/notification-providers/email.js
@@ -3,9 +3,17 @@ module.exports = ({strapi}) => ({
   async send(config, data){
     let recipients = await strapi.query('plugin::ezforms.recipient').findMany();
     let message = ""
+    function transformData(key, data) {
+      switch (key) {
+        case 'email':
+          return `<a href="mailto:${data}" target="_blank">${data}</a>`;
+        default:
+          return data;
+      }
+    }
     //Loop through data and construct message from data object
     for(let key in data){
-      message += `${key}: ${data[key]}\n`
+      message += `<strong>${key}</strong>: ${transformData(key, data[key])}<br />`
     }
     //loop through the recipients and send an email
     for(let recipient of recipients){
@@ -13,7 +21,7 @@ module.exports = ({strapi}) => ({
         await strapi.plugins['email'].services.email.send({
           to: recipient.email,
           from: config.from,
-          subject: 'New Contact Form Submission',
+          subject: 'Formulier op website: nieuwe inzending',
           text: message,
         });
       } catch (e) {
